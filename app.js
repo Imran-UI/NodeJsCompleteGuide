3a8a7cfef7f11b051d656d18ef1e15a54690ab8d
Mohammad Imran Shaik -X (mshaik4 - ZENSAR TECHNOLOGIES INC at Cisco) <mshaik4@cisco.com>
2019-02-09 16:57:41 -0800

    using middle wares

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const http = require('http');
+const express = require('express');
+
+// node js server fire up test
 
 // const server  = http.createServer((req,res) => {
 //     console.log("response ", res);
 // });
 
 // server.listen(3000);
+
+const app = express();
+//adding middle ware 
+
+app.use((req,res,next)=> {
+    console.log("in the Middle ware");
+    next();
+})
+
+app.use((req, res, next) => {
+   res.send('<h1>Hello from Node JS</h1>');
+})
+
+app.listen(3000);
+
