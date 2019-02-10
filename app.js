8eecfeeb7f67baaacef266e9ba843f5aa513ee63
Mohammad Imran Shaik -X (mshaik4 - ZENSAR TECHNOLOGIES INC at Cisco) <mshaik4@cisco.com>
2019-02-09 18:41:56 -0800

    serving files statically using public

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(shopRoutes);
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 
 app.use((req,res,next)=> {
     res.status(404);
