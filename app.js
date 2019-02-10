bc84905b7e5cafc887abfc72e380ab30a02430b1
Mohammad Imran Shaik -X (mshaik4 - ZENSAR TECHNOLOGIES INC at Cisco) <mshaik4@cisco.com>
2019-02-09 17:25:00 -0800

    adding body-parser package

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser =  require('body-parser');
 
 // node js server fire up test
 
@@ -11,12 +12,25 @@ const express = require('express');
 const app = express();
 //adding middle ware 
 
-app.use((req,res,next)=> {
-    console.log("in the Middle ware");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req,res,next)=> {
+    res.send(`
+  <form action="/products" method='POST'>
+  Enter:<br>
+  <input type="text" name="title">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
+`);
 })
 
-app.use((req, res, next) => {
+app.post("/products", (req, res, next) => {
+    console.log("req ", req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
    res.send('<h1>Hello from Node JS</h1>');
 })
 
